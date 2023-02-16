8fc33350cf931d63efab1271a129804c089af2c2
Katharina Ephan <ephankatharina@gmail.com>
2023-02-16 09:29:50 +0100

    Soleves SumApp task

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,25 @@
+function sumArray(arr) {
+  return arr.reduce((acc, val) => acc + val, 0);
+}
+
 class SumApp {
-  // Add you solution here
+  constructor(numbers) {
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    if (this.numbers.length > 0) {
+      return sumArray(this.numbers);
+    } else {
+      return 0;
+    }
+  }
+
+  reset() {
+    this.numbers = [];
+  }
 }
