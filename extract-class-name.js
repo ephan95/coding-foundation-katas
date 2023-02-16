d4a8f9e551a53f165f887b935130922cda97a768
Katharina Ephan <ephankatharina@gmail.com>
2023-02-16 10:40:46 +0100

    Solves class name task

--- extract-class-name.js
+++ extract-class-name.js
@@ -1,3 +1,34 @@
 function extractClassName(sessionTitle) {
-  // Add you solution here
+  const array = sessionTitle.split(" ");
+  const months =
+    /^(Januar|Februar|März|Maerz|April|Mai|Juni|July|August|September|Oktober|November|Dezember)$/;
+  const monthNumbers = {
+    Januar: 1,
+    Februar: 2,
+    März: 3,
+    Maerz: 3,
+    April: 4,
+    Mai: 5,
+    Juni: 6,
+    Juli: 7,
+    August: 8,
+    September: 9,
+    Oktober: 10,
+    November: 11,
+    Dezember: 12,
+  };
+
+  if (
+    array.includes("Class") &&
+    array[2].length === 4 &&
+    months.test(array[3])
+  ) {
+    return (
+      array[2].toString() +
+      "-" +
+      monthNumbers[array[3]].toString().padStart(2, "0")
+    );
+  } else {
+    return null;
+  }
 }
